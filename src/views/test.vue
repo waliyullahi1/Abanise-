5c76f5e63c8d172dc9e9c6f1c2cd475261e8b1c1
waliyullahi1 <waliuwaheed2021@gmail.com>
2023-10-21 12:24:09 -0700

    update

--- src/views/test.vue
+++ src/views/test.vue
@@ -1,68 +1,13 @@
 <template>
 
-
-  <vue3-html2pdf
-    :show-layout="false"
-    :float-layout="true"
-    :enable-download="true"
-    :preview-modal="true"
-    :paginate-elements-by-height="1400"
-    filename="Hello My friend"
-    :pdf-quality="2"
-    :manual-pagination="true"
-    pdf-format="a4"
-    pdf-orientation="portrait"
-    pdf-content-width="800px"
-    @progress="onProgress($event)"
-    @hasStartedGeneration="hasStartedGeneration()"
-    @hasGenerated="hasGenerated($event)"
-    ref="html2Pdf"
-  >
-    <template #pdf-content>
-     <section slot="pdf-content" class=" container mx-auto">
-         <div >
-            <div class=" mx-10">
-               <h1 class=" font-semibold text-2xl text-center">abaniseedu.com</h1>
-               <p class="text-[14px] font-medium py-1">YOUR TRANSACTION DETAILS ARE AS FOLLOWS:</p>
-               <p class=" text-green-500 py-5 rounded-md text-center bg-green-100">Your Successful purchased {{}}
-               </p>
-               <div class="text-[16px] bg-white  ">
-              
-              <div class="mt-10 w-full border  mb-3">
-               <ul class=" text-[13px]  font-medium" v-for="item in itemss" :key="item.id" >
-                <li class="sm:flex justify-center   block sm:border-b-0 border-b  sm:py-0  py-3 px-3 sm:gap-6 gap-2 ">
-                  <div class=" p-0 sm:p-3 w-1/3">
-                      <h1 class=" text-gray-600">{{item.headers}}</h1>
-                  </div>
-
-                  <div class="w-full p-0 sm:p-3 border-l-0 sm:border-l border-b-0 sm:border-b">
-                    <h1>{{item.details}}</h1>
-                  </div>
-                </li>
-               </ul>
-              </div>
-               <div class="flex gap-5 justify-center mb-9 mt-9">
-
-                 <button @click="printDownload">Print Download</button>
-                <Download v-show="false" ref="DownloadComp" />
-                   <Button @click="generatePdf()"  :loading="loadingState" class=" hidden w-fit h-fit px-10" loadingText="please wait...."> print</Button>
-                   <Button :loading="loadingState" class=" w-fit px-10" loadingText="please wait...."> save</Button>
-
-                 </div>
-              
-            </div>
-            </div>
-         </div>
-                </section>
-    </template>
-  </vue3-html2pdf>
-  <button @click="generateReport()">Delleor</button>
+<div>
+  <h1>ilove you</h1>
+</div>
 </template>
 
 <script>
 
-import Vue3Html2pdf from "vue3-html2pdf";
-import jspdf from 'jspdf'
+
 export default {
    data() {
     return {
@@ -70,25 +15,7 @@ export default {
     
 
     
-      text:'1',
-      input:'1',
-      loadingState2:false,
-      transacPrev:false,
-      track: true,
-      erroremail1:false,
-      errortransactionCode:false,
-      cardlink:'',
-       numm:Number(1),
-      cardName : '',
-      form: {
-        name:'',
-        email:'',
-        semiprice:'',
-        quantity:  1,
-       price:Number, 
-        total: '' , 
-        transactionpin:'',
-      },
+  
       
      
     };
@@ -101,10 +28,7 @@ export default {
     Vue3Html2pdf,
   },
   methods: {
-    /*
-            Generate Report using refs and calling the
-            refs function generatePdf()
-        */
+    
     generateReport() {
       this.$refs.html2Pdf.generatePdf();
     },
