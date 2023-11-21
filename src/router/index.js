f573d36536cca39d7ab71437e5484a94bc74ab8b
unknown <waliuwaheed2021@gmail.com>
2023-11-21 01:48:01 -0800

    upate

--- src/router/index.js
+++ src/router/index.js
@@ -13,12 +13,28 @@ const routes = [
     path: "/",
     name: "index.vue",
     component: () => import("../views/index.vue"),
-    
+    title: 'Home Page',
+      metaTags: [
+        {
+          name: 'description',
+          content: 'The home page of our example app.'
+        },
+        {
+          property: 'og:description',
+          content: 'The home page of our example app.'
+        }
+      ]
   },
   {
     path: "/aboutus",
     name: "aboutus",
     component: () => import("../views/aboutus.vue"),
+   
+  },
+  {
+    path: "/quiz",
+    name: "aboutus",
+    component: () => import("../views/qiuz.vue"),
    
   },
   {
@@ -136,10 +152,21 @@ const router = createRouter({
   routes,
 });
 router.beforeEach((to, from, next) => {
-  document.title = to.meta.title
-  
-  next()
-})
+  const nearestWithTitle = to.matched.slice().reverse().find(r => r.meta && r.meta.title);
+  const nearestWithMeta = to.matched.slice().reverse().find(r => r.meta && r.meta.metaTags);
+  if(nearestWithTitle) document.title = nearestWithTitle.meta.title;
+  Array.from(document.querySelectorAll('[data-vue-router-controlled]')).map(el => el.parentNode.removeChild(el));
+  if(!nearestWithMeta) return next();
+  nearestWithMeta.meta.metaTags.map(tagDef => {
+    const tag = document.createElement('meta');
+    Object.keys(tagDef).forEach(key => {
+      tag.setAttribute(key, tagDef[key]);
+    });
+    tag.setAttribute('data-vue-router-controlled', '');
+    return tag;
+  }).forEach(tag => document.head.appendChild(tag));
+  next();
+});
 
 
 
---
+++ src/views/qiuz.vue
@@ -0,0 +1,62 @@
+<template class=" text-[poppins]">
+    <div class=" bg-slate-200 text-[puppins] ">
+       <Header :headertext="false"  class="fixed z-40 top-0"></Header> 
+        <div class="  flex justify-center items-center  w-full h-screen">
+            <section class="bg-white shadows w-[80%] h-[60%]">
+                <div class="py-2 px-2">
+                    <div class=" h-32 py-3 px-3 bg-slate-500 w-full">
+                        <h1 class=" text-xl">what is the names of your school </h1>
+                    </div>
+                    <div>
+                        <ul>
+                            <li class=" flex gap-2  items-center">
+                                <input type="radio"  name="" id="1"><p class=" text-xl font-medium">Agbaje</p>
+                                <input type="radio"  name="" id="2"><p class=" text-xl font-medium">Agbaje</p>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </section>
+        </div>
+
+     
+    </div>
+</template>
+
+<script>
+import { findDir } from '@vue/compiler-core';
+import { format } from 'date-fns'
+ import { ref } from 'vue'
+import html2pdf from 'html2pdf.js'
+
+   export default {
+     
+
+     
+
+     
+    
+
+     methods: {
+    generatePdf() {
+      html2pdf().from(this.$refs.content).save()
+    }
+  }
+
+    
+} 
+</script>
+
+<style>
+    .shadows{
+        box-shadow: rgba(50, 50, 93, 0.25) 0px 50px 100px -20px, rgba(0, 0, 0, 0.3) 0px 30px 60px -30px;
+    }
+    input[type="radio"] {
+  width: 20px;
+  height: 20px;
+}
+
+input[type="radio"]:checked {
+  background-color: red;
+}
+</style>
